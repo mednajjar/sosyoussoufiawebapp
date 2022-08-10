8d27ab21f0c5b66d273e514081eb9a94435a616f
Mohammed <najjarmedd@gmail.com>
2022-08-10 13:33:40 +0100

    hide footer

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import {
   Routes,
   Route,
 } from "react-router-dom";
-import Footer from './components/layouts/Footer';
+// import Footer from './components/layouts/Footer';
 
 
 
@@ -21,7 +21,7 @@ function App() {
         <Route path="contact" element={<Contact />} />
         <Route path="*" exact={true} element={<NotFound />} />
       </Routes>
-      <Footer />
+      {/* <Footer /> */}
     </BrowserRouter>
   );
 }
