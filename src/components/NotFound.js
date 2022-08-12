879e7636e718cbe7a4e9142b26005a89d32a2eaf
Mohammed <najjarmedd@gmail.com>
2022-08-12 21:37:25 +0100

    update 404 page

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -3,7 +3,7 @@ import { P404 } from '../assets'
 const NotFound = () => {
   return (
     <div style={{ background: '#F6F6F6' }} className='grid font-semibold text-lg text-center max-w-7xl mx-auto mt-10 rounded-full h-full items-center justify-center p-5'>
-      <p style={{ color: '#459350' }}>Oop's! Page NotFound</p>
+      <p style={{ color: '#459350' }}>Oop's! Page Not Found</p>
       <img src={P404} alt="page-not-found" className='rounded-full' />
     </div>
   )
