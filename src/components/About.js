e384d6be347b2832e231a04958a538a3fa81d5c2
Mohammed <najjarmedd@gmail.com>
2022-08-12 21:09:46 +0100

    change p tag on about page

--- src/components/About.js
+++ src/components/About.js
@@ -19,7 +19,7 @@ const About = () => {
             <div className='max-w-7xl mx-auto'>
                 <h2 className='text-2xl font-medium text-slate-700'>حول التطبيق</h2>
 
-                <p className='text-lg font-medium text-slate-700 group-hover:text-slate-900 mt-4 leading-9'>أول نسخة من  هذا التطبيق تساعد في معرفة صيدلية الحراسة كل أسبوع وكذلك موقع تواجدها على الخريطة كما تتيح توفير أرقام الهواتف و ربط الاتتصال بسيارة الأجرة من النوع الصغير أرجو أن ينتفع به ساكنة مدينة اليوسفية</p>
+                <p className='text-lg font-medium text-slate-700 group-hover:text-slate-900 mt-4 leading-9'>أول نسخة من  هذا التطبيق تساعد في معرفة صيدلية الحراسة كل أسبوع وكذلك موقع تواجدها على الخريطة كما تتيح توفير أرقام الهواتف و ربط الاتصال بسيارة الأجرة من النوع الصغير أرجو أن ينتفع به ساكنة مدينة اليوسفية</p>
                 <p className='text-lg font-medium text-slate-700 group-hover:text-slate-900 mt-4 leading-9'>سنقوم بإضافة خدمات أخرى مستقبلا حسب الحاجة</p>
             </div>
         </div>
